b62c5be33c11172bf329b28403fecf9221272d0f
Enzo Lebrun <enzo.the@gmail.com>
2024-08-11 21:00:55 +0200

    build: deploy.yml

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,22 @@
 from queupy import init_queue, LIFOEventQueue, FIFOEventQueue
 import pytest
+from os import environ
+
+HOST = environ.get('DB_HOST', 'localhost')
+USER = environ.get('DB_USER', 'queupy')
+PASSWORD = environ.get('DB_PASSWORD', 'queupy')
+DATABASE_NAME = environ.get('DB_NAME', 'queupy')
+PORT = environ.get('DB_PORT', 5432)
 
 
 @pytest.fixture()
 def event_queue():
     event_queue = init_queue(
-        database_name='queupy',
-        host='localhost',
-        user='queupy',
-        password='queupy',
+        database_name=DATABASE_NAME,
+        host=HOST,
+        user=USER,
+        password=PASSWORD,
+        port=PORT,
     )
     yield event_queue
 
